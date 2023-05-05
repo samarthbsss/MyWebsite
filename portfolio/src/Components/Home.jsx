348dcadd552aa53c976cfdbffbe782ee0f7423f6
Samarth <samarthbsacharya@gmail.com>
2023-05-05 22:58:16 +0530

    working

---
+++ portfolio/src/Components/Home.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./Nav";
+import Main from "./Main";
+
+const Home=()=>{
+ return<>
+ <Navbar/>
+ <Main/>
+ </>
+}
+
+export default Home;
---
+++ portfolio/src/Components/Nav.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from '../Css/nav.css'
+
+const Navbar=()=>{
+    return  <nav className="navbar">
+        <h1>Value</h1>
+        <div className="buttoncluster">
+        <button>Home</button>
+        <button>Project</button>
+        <button>about</button>
+        <button>Connect</button>
+
+        </div>
+       
+        
+        </nav>
+        
+  
+}
+
+export default Navbar;
